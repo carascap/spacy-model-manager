729cb044db0c0da8965357545c039713c5bb47d9
Antoine de Torcy <adetorcy@users.noreply.github.com>
2021-12-20 22:30:20 -0500

    Rearrange dependencies

--- update_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-pip-compile -o requirements.txt requirements.in --upgrade --quiet "$@"
-pip-compile -o requirements-dev.txt requirements-dev.in requirements.in --upgrade --quiet "$@"
